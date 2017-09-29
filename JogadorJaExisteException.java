79bbe277aeb4f9584151551f5c733ac8fa37e643
Arnaldo Leite Abreu <31657852+arnaldoleiteabreu@users.noreply.github.com>
2017-09-29 10:53:43 -0300

    Delete JogadorJaExisteException.java

--- JogadorJaExisteException.java
+++
@@ -1,9 +0,0 @@
-
-public class JogadorJaExisteException extends Exception {
-	private static final long serialVersionUID = 1L;
-	public JogadorJaExisteException(String msg) {
-		super(msg);
-	}
-
-
-}
